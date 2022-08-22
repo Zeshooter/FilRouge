e8e08719685213ddc53bd5a6c07a2396795bfef9
Zeshooter <romainsoulie1973@gmail.com>
2022-08-22 19:44:09 +0200

    Update

--- PositivumDjango/positivum/blog/urls.py
+++ PositivumDjango/positivum/blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug>/', views.article_detail, name='article_detail'),
+    # path ('', views.article_liste, name='index'),
+    # path ('liste.html', views.article_liste, name='liste'),
+    path ('<slug>/', views.article_detail, name='article_detail'),
+    path('', views.ArticleListeVue.as_view(), name='article_liste'),
 ]
--- PositivumDjango/positivum/blog/views.py
+++ PositivumDjango/positivum/blog/views.py
@@ -1,15 +1,31 @@
+from multiprocessing import context
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from .models import Oeuvre, Article
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views import generic
 
 # Create your views here.
 
-def index(request):
+def article_liste (request):
     oeuvres = Oeuvre.objects.all()
-    articles = Article.objects.all()
-    print(articles)
-    return render(request, 'blog/index.html', {"oeuvres": oeuvres, 'articles': articles})
+    # object_list = Article.objects.all()
+    # paginator = Paginator(object_list, 3)
+    # page = request.GET.get('page')
+    # try :
+    #     articles = paginator.page(page)
+    # except PageNotAnInteger :
+    #     articles = paginator.page(1)
+    # except EmptyPage :
+    #     articles = paginator.page(paginator.num_pages)
+    # context = {'articles':articles, 'page':page, 'oeuvres':oeuvres}
+    return render(request, 'blog/articles/liste.html', {'oeuvres':oeuvres})
 
 def article_detail(request, slug: str):
     article = get_object_or_404(Article, slug_article=slug)
     return render(request, 'blog/articles/detail_article.html', {'article': article})
+
+class ArticleListeVue(generic.ListView):
+    queryset = Article.objects.all()
+    paginate_by = 3
+    template_name = 'blog/articles/liste.html'
+    context_object_name = "articles"
